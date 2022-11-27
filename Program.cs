eb28c1994cadb94ca6c825bbe3374f1cdecb1e28
ashokk <ashokk@nextgen.com>
2022-11-27 18:05:37 +0530

    added initial code

---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace Exceptions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Username:");
+            string username = Console.ReadLine();
+            Console.WriteLine("Enter Password:");
+            string Password = Console.ReadLine();
+
+            try
+            {
+                var NameInOurDatabase = "";
+                if ( !NameInOurDatabase.Contains(username))
+                {
+                    throw new UserNameInvalidException(username + " is not in our database");
+                }
+                if (Password != "123456")
+                {
+                    throw new UserPasswordInvalidException("Password was wrong");
+                }
+            }
+            catch
+            {
+                throw;
+            }
+            //catch(UserNameInvalidException e)
+            //{
+            //    Console.WriteLine(e.Message);
+            //}
+            //catch(UserPasswordInvalidException p)
+            //{
+            //    Console.WriteLine(p.Message);
+            //}
+            //catch (Exception e)
+            //{
+            //    Console.WriteLine(e.Message);
+            //}
+            Console.ReadKey();
+        }
+    }
+    public class UserNameInvalidException : Exception
+    {
+
+        public UserNameInvalidException(string message):base(message)
+        {
+        }
+    }
+    public class UserPasswordInvalidException : Exception
+    {
+        public UserPasswordInvalidException(string message): base(message)
+        {
+
+        }
+    }
+}
